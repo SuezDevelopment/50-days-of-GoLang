2c2dfb0ecf456eec8beb5bc8fd7177f3a5b02ece
Gideon Olayode <99191347+SuezDevelopment@users.noreply.github.com>
2023-01-24 01:51:54 +0800

    Create voice_chat.go

---
+++ Conference voice chatting/voice_chat.go
@@ -0,0 +1,54 @@
+package voice_chat
+
+import (
+    "github.com/pion/webrtc/v2"
+)
+
+// Create a new WebRTC peer connection
+
+peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{
+    ICEServers: []webrtc.ICEServer{
+        {
+            URLs: []string{"stun:stun.l.google.com:19302"},
+        },
+    },
+})
+if err != nil {
+    log.Fatalf("Failed to create peer connection: %v", err)
+}
+
+// Create a new audio track for the local user
+audioTrack, err := peerConnection.NewTrack(webrtc.DefaultPayloadTypeOpus, rand.Uint32(), "audio", "pion")
+if err != nil {
+    log.Fatalf("Failed to create audio track: %v", err)
+}
+
+//Add the audio track to the peer connection
+if _, err = peerConnection.AddTrack(audioTrack); err != nil {
+    log.Fatalf("Failed to add audio track: %v", err)
+}
+
+// Create an offer and send it to the other users
+
+offer, err := peerConnection.CreateOffer(nil)
+if err != nil {
+    log.Fatalf("Failed to create offer: %v", err)
+}
+if err = peerConnection.SetLocalDescription(offer); err != nil {
+    log.Fatalf("Failed to set local description: %v", err)
+}
+
+// Handle incoming answers and ICE candidates from the other users
+
+peerConnection.OnICECandidate(func(candidate *webrtc.ICECandidate) {
+    // Send the candidate to the other users
+})
+peerConnection.OnTrack(func(track *webrtc.Track, receiver *webrtc.RTPReceiver) {
+    // Handle the incoming track
+})
+
+// Start the audio track and wait for the other users to join
+if err = audioTrack.Start(); err != nil {
+    log.Fatalf("Failed to start audio track: %v", err)
+}
+
