43c5b1c308c003102b6162f0c736e16f7e7bf6d8
Gideon Olayode <99191347+SuezDevelopment@users.noreply.github.com>
2023-01-24 00:11:56 +0800

    Update chat.go

--- Direct & Group Chatting/chat.go
+++ Direct & Group Chatting/chat.go
@@ -1,3 +1,68 @@
 package go_chat
 
 // websocket chatting...
+
+import (
+    "log"
+    "net/http"
+    "github.com/gorilla/websocket"
+)
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan []byte)
+
+var upgrader = websocket.Upgrader{
+    CheckOrigin: func(r *http.Request) bool {
+        return true
+    },
+}
+
+func main() {
+    http.HandleFunc("/ws", handleConnections)
+
+    go handleMessages()
+
+    log.Println("server started on :8000")
+    err := http.ListenAndServe(":8000", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+    ws, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer ws.Close()
+
+    clients[ws] = true
+
+    for {
+        var msg []byte
+        err := ws.ReadJSON(&msg)
+        if err != nil {
+            log.Printf("error: %v", err)
+            delete(clients, ws)
+            break
+        }
+
+        broadcast <- msg
+    }
+}
+
+func handleMessages() {
+    for {
+        msg := <-broadcast
+
+        for client := range clients {
+            err := client.WriteJSON(msg)
+            if err != nil {
+                log.Printf("error: %v", err)
+                client.Close()
+                delete(clients, client)
+            }
+        }
+    }
+}
