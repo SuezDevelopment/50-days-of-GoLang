0534f71d3eda6dced2f541e81c01e85585cd4f91
Gideon Olayode <99191347+SuezDevelopment@users.noreply.github.com>
2023-02-08 12:31:16 +0100

    Create download_time.go

---
+++ Download time/download_time.go
@@ -0,0 +1,15 @@
+/*
+The download time depends on several factors such as the speed of the server, network congestion, etc
+*/
+// calculate the estimated download time using the average download speed
+
+package download_time
+
+func DownloadTime(file_size int, download_speed int) int {
+  
+  //convert the file size and internet speed to bits
+  file_size_in_bits := file_size * 1024 * 1024 * 1024 * 8
+  download_speed_in_bits_per_second := download_speed * 1024 * 1024
+  
+  return file_size_in_bits/download_speed_in_bits_per_second
+}
